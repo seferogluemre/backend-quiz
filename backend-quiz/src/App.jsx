684b67ba6b56deed703e14482b18efd5ccf28534
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-16 00:11:10 +0300

    adding answers to questions

--- backend-quiz/src/App.jsx
+++ backend-quiz/src/App.jsx
@@ -5,7 +5,6 @@ import Aperture from './assets/components/icons/Aperture'
 import Compass from './assets/components/icons/Compas'
 import CPU from './assets/components/icons/Cpu'
 
-
 function App() {
 
   const [selectedImage, setSelectedImage] = useState(0);
@@ -13,13 +12,20 @@ function App() {
   const cards = [
     { id: 1, name: '1-3', image: () => <Activity /> },
     { id: 2, name: '4-18', image: () => <Aperture /> },
-    { id: 3, name: '19-35', image: () => <Compass /> },
-    { id: 4, name: '36-46', image: () => <CPU /> }
-  ]
+    { id: 3, name: '19-27', image: () => <CPU /> },
+    { id: 4, name: '28-35', image: () => <Compass /> },
+    { id: 5, name: '36-40', image: () => <CPU /> },
+    { id: 6, name: '41-46', image: () => <Activity /> }
+  ];
 
   const questionPhotos = [
     { id: 1, photoSrc: "/src/assets/components/questionPhotos/cevap1.png" },
-  ]
+    { id: 2, photoSrc: "/src/assets/components/questionPhotos/cevap2.png" },
+    { id: 3, photoSrc: "/src/assets/components/questionPhotos/cevap3.png" },
+    { id: 4, photoSrc: "/src/assets/components/questionPhotos/cevap4.png" },
+    { id: 5, photoSrc: "/src/assets/components/questionPhotos/cevap5.png" },
+    { id: 6, photoSrc: "/src/assets/components/questionPhotos/cevap6.png" },
+  ];
 
   return (
     <>
